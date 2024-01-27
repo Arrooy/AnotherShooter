8ffb40d237bfe3baf2936aef5b8e9dd78672d419
Arroyo <adria.arroyo@outlook.es>
2024-01-27 14:54:45 +0100

    roaming of npc addeed

--- NPC.py
+++ NPC.py
@@ -1,6 +1,6 @@
 from Entity import EntityWithItems
 from __init__ import Game
-import random
+from random import random
 import time
 import math
 
@@ -14,6 +14,10 @@ class NPC(EntityWithItems):
         self.attack_speed_ms = attack_speed
         self.last_attack_time = 0
         
+        self.state = 0
+        self.last_state = 0
+        self.last_roam = 0
+        
         Game.npcs[self.id] = self
         
         if "npcs" not in Game.new_stuff:
@@ -23,26 +27,76 @@ class NPC(EntityWithItems):
         
         
     def update(self):
+        if self.hp <= 0:
+            self.hp = 0
+            self.dropItems()
+            self.toRemove = True 
+            return
+        
+        closest_player, distance = self.find_closest_player()
+        
         # Add logic here
-        self.behave()
+        self.behave(closest_player, distance)
         super().update()
+        
         # Check colision with other ColisionEntities.
+        if closest_player is not None and self.check_colision_stop(closest_player):
+            # Touched a player
+            self.attack(closest_player)
+        
+        """
         for i in Game.players:
             if self.check_colision_stop(Game.players[i]):
                 # Touched a player
                 self.attack(Game.players[i])      
+        """
         
         # Repel otrher npcs
         for i in Game.npcs:
             self.check_colision_repel(Game.npcs[i])
         
-        if self.hp <= 0:
-            self.hp = 0
-            self.dropItems()
-            self.toRemove = True 
+       
     
-    def behave(self):
-        # Find closest player in sight (500)
+    def behave(self, closest_player, distance):
+        if closest_player is not None and distance < 500:
+            self.state = 4
+            
+        if self.state == 0:
+            # Stop
+            self.vx = 0
+            self.vy = 0
+            if time.time_ns() - self.last_roam > 1_000_000 * (random() * 1000 + 500):
+                self.last_roam = time.time_ns()
+                self.state = 1
+                
+        elif self.state == 1:
+            self.vx = random() * self.max_speed - self.max_speed/2
+            self.vy = random() * self.max_speed - self.max_speed/2
+            self.last_roam = time.time_ns()
+            self.state = 3
+        elif self.state == 3:
+            # Roam
+            if time.time_ns() - self.last_roam > 1_000_000 * (random() * 1000 + 500):
+                self.last_roam = time.time_ns()
+                self.state = 0
+            
+        elif self.state == 4:
+            # Follow
+            isFollowing = self.follow(closest_player, distance)
+            if not isFollowing:
+                self.state = 0
+        
+    def follow(self, closest_player, min_distance):
+        if closest_player is not None and min_distance != 0 and min_distance < 500:
+            ## Found a player, follow it.
+            self.vx = self.max_speed * (closest_player.x - self.x)/min_distance
+            self.vy = self.max_speed * (closest_player.y - self.y)/min_distance
+            return True
+        else:
+            return False
+    
+    def find_closest_player(self):
+        # Find closest player
         min_distance = None
         closest_player = None
         for i in Game.players:
@@ -55,13 +109,7 @@ class NPC(EntityWithItems):
                 closest_player = player
                 min_distance = distance
         
-        if closest_player is not None and min_distance != 0 and min_distance < 500:
-            ## Found a player, follow it.
-            self.vx = self.max_speed * (closest_player.x - self.x)/min_distance
-            self.vy = self.max_speed * (closest_player.y - self.y)/min_distance
-        else:
-            self.vx = 0
-            self.vy = 0
+        return closest_player, min_distance
     
     
     def attack(self, player):
